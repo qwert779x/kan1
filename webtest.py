c852a6fb0b1884f63541958b47bdc9d08a15c7ec
qwert779 <kacper.blakala@gmail.com>
2024-10-02 09:35:38 +0200

    dodana funkcja sprawdzania koloru

--- webtest.py
+++ webtest.py
@@ -1,4 +1,7 @@
 import unittest
+from os.path import split
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,7 +11,7 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def test_search(self):
+    def test_title(self):
         driver = self.driver
         driver.get("http://www.python.org")
         self.assertIn("Python", driver.title)
@@ -16,6 +19,12 @@ class PythonOrgSearch(unittest.TestCase):
         elem.send_keys("pycon")
         elem.send_keys(Keys.RETURN)
         self.assertNotIn("No results found.", driver.page_source)
+
+    def test_bgcolor(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        elem = driver.find_element(By.CSS_SELECTOR, "div.top-bar").value_of_css_property("background-color")
+        print("TextColor of the Element: " + elem)
 
     def tearDown(self):
         self.driver.close()
