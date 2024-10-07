54af0d4498d9a654e5b73923ddec4a4b41aac465
qwert779 <kacper.blakala@gmail.com>
2024-10-07 10:03:35 +0200

    correction of code

--- kan1-main/kan1-main/webtest.py
+++ kan1-main/kan1-main/webtest.py
@@ -1,45 +1,21 @@
-import unittest
-from os.path import split
-
-
+import pytest
 from selenium import webdriver
-# from selenium.webdriver.common.actions.action_builder import ActionBuilder
-# from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-# TODO: Dodanie pliku gitignore w celu zignorowania niepotrzebnych plików
-# Zmienienie sposobu intrakcji na bloki ActionChains dla poprawienia czytelności kodu. Przykład:
-# ActionChains(self.driver)\
-#     .move_to_element(elem)\
-#     .send_keys("pycon")\
-#     .key_down(Keys.RETURN)\
-#     .perform()
-# ActionBuilder.clear_actions()
-# self.assertNotIn("No results found.", driver.page_source)
+@pytest.fixture
+def driver():
+    # Setup: create a new instance of the Chrome driver
+    driver = webdriver.Chrome()
+    yield driver  # This will be used in the tests
+    # Teardown: close the driver
+    driver.quit()
 
-class PythonOrgSearch(unittest.TestCase):
+def test_title_if_correct(driver):
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    assert "No results found." not in driver.page_source
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-
-    def test_title(self):
-        driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element(By.NAME, "q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        self.assertNotIn("No results found.", driver.page_source)
-
-    def test_bgcolor(self):
-        driver = self.driver
-        driver.get("http://www.python.org")
-        elem = driver.find_element(By.CSS_SELECTOR, "div.top-bar").value_of_css_property("background-color")
-        print("TextColor of the Element: " + elem)
-
-    def tearDown(self):
-        self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_bgcolor_is_correct(driver):
+    driver.get("http://www.python.org")
+    elem = driver.find_element(By.CSS_SELECTOR, "div.top-bar").value_of_css_property("background-color")
+    assert "rgba(30, 41, 51, 1)" in elem
